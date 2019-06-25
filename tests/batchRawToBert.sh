1f5c36cfc9fdac0c4f40b8bb3339bec9a4e9f8e1
Ethan Chi <ethanchi@stanford.edu>
2019-06-25 13:53:50 -0700

    fixes

--- tests/batchRawToBert.sh
+++ tests/batchRawToBert.sh
@@ -5,8 +5,8 @@ do
     for f in $1/*.txt
     do
         echo "Processing $f $type"
-        echo "Path is: ${f/.txt/-$type.hdf5}"
-        #python3 ../scripts/convert_raw_to_bert.py "$f" "${f/.txt/-type-hdf5}" "$type" 
+        #echo "Path is: ${f/.txt/-$type.hdf5}"
+        python3 ../scripts/convert_raw_to_bert.py "$f" "${f/.txt/-$type.hdf5}" "$type" 
     done
 done
 
